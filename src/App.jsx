e3aa11f66e8a141deb1d6b8e15581ee99167e41d
Ben Hardy <hardybe92@gmail.com>
2022-03-27 15:19:39 -0600

    Added 5 more rows for the 6 guesses and added another state to manage which row the game is on. next I need to add a means of showing the game status within those 6 rows. will probably make each letter a component that can have its background colour controlled.

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,121 @@
-import { useState } from 'react'
+import { useState, useCallback, useEffect } from 'react'
 import logo from './logo.svg'
 import './App.css'
 
-function App() {
-  let guess = ["_", "_", "_", "_", "_"];
+function App(props) {
+  let guess = [["_", "_", "_", "_", "_"],
+                ["_", "_", "_", "_", "_"],
+                ["_", "_", "_", "_", "_"],
+                ["_", "_", "_", "_", "_"],
+                ["_", "_", "_", "_", "_"],
+                ["_", "_", "_", "_", "_"]];
   const [letters, setLetters] = useState(guess);
   const [curLetter, setCurLetter] = useState(0);
+  const [curGuess, setCurGuess] = useState(0);
   let cl = 0;
-  let guessLetter = ""
+  let cg = 0;
+  let guessLetter = "";
 
-  
   function updateState() {
     cl = curLetter;
+    cg = curGuess;
     let updatedGuess = [...letters];
-    if (updatedGuess[cl] === "_") {
-      updatedGuess[cl] = guessLetter;
+    if (updatedGuess[cg][cl] === "_") {
+      updatedGuess[cg][cl] = guessLetter;
     }
 
     if (cl < 4) {
       cl++;
     }
+
+    if (cg < 6) {
+      cg++;
+    }
     console.log(updatedGuess);
     setLetters(updatedGuess);
     setCurLetter(cl);
+    
+  }
+
+  /*const keyInputHandler = useCallback((event) => {
+    const alphabet = "abcdefghijklmnopqrstuvwxyz";
+    if (alphabet.includes(event.key)) {
+      guessLetter = event.key;
+      updateState();
+      console.log(event.key);
+    }
+
+    if (event.key === "Backspace") {
+      backSpacePressed();
+    }
+
+  })*/
+/*
+  useEffect(() => {
+    const keyInputHandler = (e) => {
+      const alphabet = "abcdefghijklmnopqrstuvwxyz";
+      if (alphabet.includes(e.key)) {
+        guessLetter = e.key;
+        updateState();
+        console.log(e.key);
+      }
+
+      if (e.key === "Backspace") {
+        backSpacePressed();
+      }
+      
+      if (e.key === "Enter") {
+        enterPressed();
+      }
+    };
+    document.addEventListener("keydown", keyInputHandler, false);
+    
+    return () => window.removeEventListener('keydown', keyInputHandler);
+  }, []);*/
+
+
+  function backSpacePressed() {
+    cl = curLetter;
+    cg = curGuess;
+    let updatedGuess = [...letters];
+    updatedGuess[cg][cl] = "_";
+
+    if (cl > 0) {
+      cl--;
+    }
+    console.log(updatedGuess);
+    setLetters(updatedGuess);
+    setCurLetter(cl)
+  }
+  
+  function enterPressed() {
+    let guessWord = [...letters][curGuess].join('');
+    cg = curGuess;
+
+    if (!guessWord.includes("_")) {
+      console.log(guessWord);
+      if (cg < 5) {
+        cg++;
+        setCurGuess(cg)
+        setCurLetter(0);
+      }
+    }
   }
 
   return (
+    
     <div className='font-mono'>
       <div className="text-3xl font-bold">
-        {letters[0]}{letters[1]}{letters[2]}{letters[3]}{letters[4]}
+        {letters[0][0]}{letters[0][1]}{letters[0][2]}{letters[0][3]}{letters[0][4]}
+        <br/>
+        {letters[1][0]}{letters[1][1]}{letters[1][2]}{letters[1][3]}{letters[1][4]}
+        <br/>
+        {letters[2][0]}{letters[2][1]}{letters[2][2]}{letters[2][3]}{letters[2][4]}
+        <br/>
+        {letters[3][0]}{letters[3][1]}{letters[3][2]}{letters[3][3]}{letters[3][4]}
+        <br/>
+        {letters[4][0]}{letters[4][1]}{letters[4][2]}{letters[4][3]}{letters[4][4]}
+        <br/>
       </div>
       
       <button onClick={() => {
@@ -83,18 +170,7 @@ function App() {
         updateState();
       }}
       className={"border-2 rounded-md px-1 hover:bg-slate-100"}>P</button>
-      <button onClick={() => {
-        cl = curLetter;
-        let updatedGuess = [...letters];
-        updatedGuess[cl] = "_";
-
-        if (cl > 0) {
-          cl--;
-        }
-        console.log(updatedGuess);
-        setLetters(updatedGuess);
-        setCurLetter(cl)
-      }}
+      <button onClick={backSpacePressed}
       className={"border-2 rounded-md px-1 hover:bg-slate-100"}>{"<--"}</button>
       <br/>
       <button onClick={() => {
@@ -112,40 +188,43 @@ function App() {
         updateState();
       }}
       className={"border-2 rounded-md px-1 hover:bg-slate-100"}>D</button>
+
       <button onClick={() => {
         guessLetter = "F";
         updateState();
       }}
       className={"border-2 rounded-md px-1 hover:bg-slate-100"}>F</button>
+
       <button onClick={() => {
         guessLetter = "G";
         updateState();
       }}
       className={"border-2 rounded-md px-1 hover:bg-slate-100"}>G</button>
+
       <button onClick={() => {
         guessLetter = "H";
         updateState();
       }}
       className={"border-2 rounded-md px-1 hover:bg-slate-100"}>H</button>
+
       <button onClick={() => {
         guessLetter = "J";
         updateState();
       }}
       className={"border-2 rounded-md px-1 hover:bg-slate-100"}>J</button>
+
       <button onClick={() => {
         guessLetter = "K";
         updateState();
       }}
       className={"border-2 rounded-md px-1 hover:bg-slate-100"}>K</button>
+
       <button onClick={() => {
         guessLetter = "L";
         updateState();
       }}
       className={"border-2 rounded-md px-1 hover:bg-slate-100"}>L</button>
-      <button onClick={() => {
-        let guessWord = [...letters].join('');
-        console.log(guessWord);
-      }}
+      <button onClick={enterPressed}
       className={"border-2 rounded-md px-1 hover:bg-slate-100"}>enter</button>
       <br/>
       <button onClick={() => {
